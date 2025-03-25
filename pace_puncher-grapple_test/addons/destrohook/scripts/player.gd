e84fb4f0c56a0950d506bcabb886faf0129df9e2
FR0Z3N-M1R4G3 <gkiefer27@bfhsla.org>
2025-03-25 13:41:09 -0500

    attack arm

--- pace_puncher-grapple_test/addons/destrohook/scripts/player.gd
+++ pace_puncher-grapple_test/addons/destrohook/scripts/player.gd
@@ -27,8 +27,12 @@ var fist_available : bool
 
 @export var slow_speed = 15.0##when the player is moving too slowly
 @export var medium_speed = 20.0##when the player is moving a bit
+
+@onready var melee = $Camera/Attack/AnimationPlayer
+@onready var hitbox = $Camera/Attack/StaticBody3D/CollisionShape3D
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
 	
 func _physics_process(delta: float) -> void:
 	# Horizontal movement
@@ -40,6 +44,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		max_speed = 15.0#speed when on floor
 
+	if Input.is_action_just_pressed("arm_use"):
+			$Camera/Attack/AnimationPlayer.play("attack")
 
 	if is_on_floor():#this changes how much control the player has while on the floor, while swinging around(dependant on speed), you want it low so the player cant just walk upwards
 		control = max_control
